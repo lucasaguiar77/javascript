73cc23d7dffc5cc70b2eac70eebaaf97af29e701
lucasaguiar77 <lucas77358@gmail.com>
2026-06-10 11:34:17 -0300

    -_-

--- exercicios/aula12ex/script.js
+++ exercicios/aula12ex/script.js
@@ -1,19 +1,21 @@
-function carregar(){
+window.onload = carregar
+function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
-    //var hora = data.getHours()
-    var hora = 22
+    var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
-    if (hora >= 0 && hora < 12) {
+    if (hora > 0 && hora < 12) {
         // BOM DIA!
-        img.src = 'fotomanha.jpg'
+        img.src = ('fotomanha.jpg')
         document.body.style.background = '#e2cd9f'
     } else if (hora >= 12 && hora < 18){
         // BOA TARDE!
-        img.src = 'fototarde.jpg'
+        img.src = ('fototarde.jpg')
+        document.body.style.background = '#b9846f'
     } else{
         // BOA NOITE!
-        img.src = 'fotonoite.jpg'
+        img.src = ('fotonoite.jpg')
+        document.body.style.background = '#515154'
     }
 }
